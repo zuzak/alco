164ef1acdef706d5ae5ea363a56440bbdf2451e1
Douglas Gardner <douglas@chippy.ch>
2015-11-29 02:17:47 +0000

    add basic tests

--- test/models/wine_test.rb
+++ test/models/wine_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class WineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    wine = Wine.new
+
+    test "a wine with no properties is invalid" do
+        assert wine.invalid? # empty wine should fail
+    end
+
+    test "price of wine should be positive" do
+        wine.price = -1
+        assert wine.invalid?, "negative prices shouldn't work"
+
+        wine.price = 0
+        assert wine.invalid?, "wines shouldn't be free"
+    end
+
+    test "price of wine should only have two decimal places" do
+        wine.price = 0.001
+        assert wine.invalid?
+    end
 end
